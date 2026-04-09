9f4ac956227ce4ee5284958531bd92cf63f6e59c
Saad <saadmuizz@gmail.com>
2026-04-08 20:08:33 -0400

    u

--- src/ApplicationConstants.ts
+++ src/ApplicationConstants.ts
@@ -18,8 +18,6 @@ export class ApplicationConstants {
   static USER_LIST_UNMOUNTED = "userListUnmounted";
   static USER_FORM_UNMOUNTED = "userFormUnmounted";
   static USER_ROLE_UNMOUNTED = "userRoleUnmounted";
-
-  static API_URL = Platform.OS === "android" ? "http://10.0.2.2" : "http://127.0.0.1";
 }
 
 export type ParamList = {
